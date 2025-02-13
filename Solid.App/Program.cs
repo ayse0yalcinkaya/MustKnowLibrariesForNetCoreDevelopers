ed997b23b32f909deadfb35a7be2c7250d50d9a3
Ayse Yalcinkaya <157410355+ayse0yalcinkaya@users.noreply.github.com>
2025-02-13 13:32:02 +0300

    Solid Principle is done

--- Solid.App/Program.cs
+++ Solid.App/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Solid.App.ISPGoodAndBad;
 using Solid.App.LSP.Good;
+using Solid.App.DIPGoodAndBad;
 
 
 Console.WriteLine("Hello, World!");
@@ -39,9 +41,15 @@ Console.WriteLine("Hello, World!");
 
 
 //LSP.Good
-BasePhone phone = new IPhone();
-phone.Call();
-((ITakePhoto)phone).TakePhoto();
+//BasePhone phone = new IPhone();
+//phone.Call();
+//((ITakePhoto)phone).TakePhoto();
 
-phone = new Nokia3310();
-phone.Call();
+//phone = new Nokia3310();
+//phone.Call();
+
+
+//DIP.GoodAndBad
+var ProductService = new ProductService(new ProductRepositoryFromOracle());
+
+ProductService.GetAll().ForEach(x => Console.WriteLine(x));
